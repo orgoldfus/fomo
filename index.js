78e7c4fd9ef2c163c7fcf4a5e8e67faf95d0096f
Or Goldfus <orgoldfus@Ors-MacBook-Pro-2.local>
2020-06-16 22:36:15 +0300

    Handle fetch failures

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
 #!/usr/bin/env node
 const { Command } = require("commander")
-const packageDetails = require("./package.json")
-const providers = require("./providers")
+const Configstore = require("configstore")
+const packageJson = require("./package.json")
+const availableProviders = require("./providers")
 const chalk = require("chalk")
 const ora = require("ora")
 
-const availableSources = providers.map(provider => `${provider.id} (${provider.name})`).join(', ')
+const DEFAULT_LIMIT = 10
 
+const config = new Configstore(packageJson.name)
 const program = new Command()
-program.version(packageDetails.version)
+
+const availableSources = availableProviders
+  .map((provider) => `${provider.id} (${provider.name})`)
+  .join(", ")
+
+program.version(packageJson.version)
 
 program
-  .option("-i, --interactive", "show the news interactively")
-  .option("-c, --configure", "configure fomo")
-  .option("-s, --source <id>", `choose a specific news source. Available sources are: ${availableSources}`)
+  // .option("-i, --interactive", "show the news interactively")
+  // .option("-c, --configure", "configure fomo")
+  .option("-l, --limit <number>", "limit the number of responses per source")
+  .option(
+    "-s, --source <source id>",
+    `choose a specific news source. Available sources are: ${availableSources}`
+  )
 
 program.parse(process.argv)
 
-if(program.source) {
+const numOfItems = parseInt(program.limit) || DEFAULT_LIMIT
 
+if (program.source) {
+  const requiredProviders = availableProviders.filter(
+    (provider) => provider.id === program.source
+  )
+  showData(requiredProviders, numOfItems, config)
 } else {
-  showData()
+  showData(availableProviders, numOfItems, config)
 }
 
-async function showData() {
+async function showData(providers, numOfItems, config) {
   for (const provider of providers) {
     const spinner = ora(`Loading ${provider.name}`).start()
-    const items = await provider.fetchItems()
-    spinner.stop()
 
-    console.log(chalk.black.bgYellow(`${provider.name}:`))
+    try {
+      const items = await provider.fetchItems({ numOfItems, config })
+      spinner.stop()
 
-    for (const item of items) {
-      console.log(chalk.yellow("●"), item)
+      console.log(chalk.black.bgYellow(`${provider.name}:`))
+
+      for (const item of items) {
+        console.log(chalk.yellow("●"), item)
+      }
+
+      console.log("\n")
+    } catch (err) {
+      spinner.fail(`Failed fetching from ${provider.name}`)
     }
-
-    console.log("\n")
   }
 }
