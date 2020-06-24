9430de5a8d6c95bd040bd54d404ca4c8f5fb29c7
Or Goldfus <orgoldfus@Ors-MacBook-Pro-2.local>
2020-06-24 22:09:07 +0300

    New provider: Reddit

--- providers/index.js
+++ providers/index.js
@@ -1,11 +1,13 @@
 const hackerNews = require("./hackerNews")
 const productHunt = require("./productHunt")
 const techCrunch = require("./techCrunch")
+const reddit = require("./reddit")
 const wired = require("./wired")
 
 module.exports = [
   hackerNews, 
   productHunt,
   techCrunch,
+  reddit,
   wired
 ]
